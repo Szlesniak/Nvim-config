8084914a0e7bf5b222a6a384aecc7b6d61272b29
Szlesniak <szymles44@gmail.com>
2025-10-31 20:17:46 +0100

    Plugins completed now

--- lua/lua/plugins/spec3.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "folke/snacks.nvim",
-    priority = 100,
-    lazy = false,
-    opts = {},
-
-  },
-}
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,29 +1,44 @@
 return {
-  {
-    -- Plugin 1: Instalator serwerów
-    "williamboman/mason.nvim",
-    opts = {
-      ui = { border = "rounded" },
-      PATH = "prepend",
-      ensure_installed = {
-        "lua_ls",
-        "clangd",
-        "tsserver",
-        "pyright",
-        "vtsls"
-
-      },
-    },
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-    opts = {
-      ensure_installed = { "lua_ls", "clangd", "tsserver", "pyright"},
-    },
-  },
-
-  {
-       "neovim/nvim-lspconfig",
-  },
+	{
+		-- Plugin 1: Instalator serwerów
+		"williamboman/mason.nvim",
+		opts = {
+			ui = { border = "rounded" },
+			PATH = "prepend",
+			ensure_installed = {
+				"lua_ls",
+				"clangd",
+				"pyright",
+				"vtsls",
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+		opts = {
+			ensure_installed = { "lua_ls", "clangd", "pyright", "vtsls" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+			})
+			vim.lsp.config("clangd", {
+				capabilities = capabilities,
+			})
+			vim.lsp.config("pyright", {
+				capabilities = capabilities,
+			})
+			vim.lsp.config("vtsls", {
+				capabilities = capabilities,
+			})
+		end,
+	},
 }
